6aa4652f438f8344f92ea0d7398994f9964f1acb
tobischw <tschwei@siue.edu>
2019-12-14 13:25:32 -0600

    Add Redux fluff to client

--- src/components/Channel/AudioClient.js
+++ src/components/Channel/AudioClient.js
@@ -1,14 +1,36 @@
 import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
 class AudioClient extends React.Component {
-
     componentDidMount() {
         console.log("Mounted audio client.");
+
+        // TODO: Start audio context here.
     }
 
     render() {
-      return <div></div>
+        return null;
     }
 }
 
-export default AudioClient;
+const mapStateToProps = state => {
+    return {
+      client: state.client
+    };
+  };
+  
+  const mapDispatchToProps = dispatch => {
+    return bindActionCreators(
+      {
+        /*toggleThemeMode: checked => toggleThemeMode(checked)*/
+      },
+      dispatch
+    );
+  };
+  
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(AudioClient);
+  
