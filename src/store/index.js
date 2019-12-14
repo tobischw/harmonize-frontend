6190a19d5c1e2598895415f4f47a1e1e2c791096
tobischw <tschwei@siue.edu>
2019-12-14 14:36:37 -0600

    Trying to figure out how to add sockets middleware

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,10 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
 import rootReducer from "./reducers";
-import reduxWebsocket from '@giantmachines/redux-websocket';
 
-// Create the middleware instance for web sockets.
-const reduxWebsocketMiddleware = reduxWebsocket();
 
-export default createStore(rootReducer, applyMiddleware(reduxWebsocketMiddleware, logger));
+
+export default createStore(
+        rootReducer, 
+        applyMiddleware(logger)
+    );
